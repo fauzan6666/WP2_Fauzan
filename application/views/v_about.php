e8aac91a885d55d72a1ea5fe0f0146e689900a83
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-05-11 23:30:41 +0700

    menambahkan .htaccess application/ assets/ composer.json index.php license.txt readme.rst system/

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fauzan</li>
+        <li>Nama Belakang : Fatoni Salim</li>
+    </ul> 
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>LONDON, ENGLISH</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pamanukan , Jawa Barat</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Swimming pool</li>
+        <li>Play FootBall</li>
+        <li>Gamers</li>
+    </ul>
+</section>
